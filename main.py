135b5f57a8d678ac97159111894a17d860f9283e
xLukxs053x <professionellxlukxs@gmail.com>
2026-05-26 19:04:59 +0200

    main.py

--- main.py
+++ main.py
@@ -10702,6 +10702,7 @@ def send_tour_start_once_for_active_tour(user_doc, telemetry, current_job_key=No
             },
             "$setOnInsert": {
                 "created_at": now,
+                "start_request_count": 0,
             },
             "$inc": {"start_request_count": 1},
         },
@@ -11257,6 +11258,7 @@ def store_tracker_webhook_start_job(payload, raw_data=None):
             },
             "$setOnInsert": {
                 "created_at": now,
+                "start_request_count": 0,
             },
             "$inc": {"start_request_count": 1},
         },
